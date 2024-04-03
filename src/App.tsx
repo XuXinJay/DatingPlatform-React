8df74621c02c389a83b313c9a41957dbe30c1cbc
XuXinJie <vv19950429@gmail.com>
2024-04-03 12:28:35 +0800

    (調整) 首頁樣式修正

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
       <Routes>
         <Route index path="/" element={<Home />} />
       </Routes>
