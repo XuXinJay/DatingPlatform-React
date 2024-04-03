6dc3bbe04a277b60f2e85ac18fc1d0c819de25a2
Oscar Wang <>
2024-04-03 12:23:51 +0800

    (調整）修正Error 警告

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
