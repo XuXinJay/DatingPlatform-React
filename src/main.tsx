a14e592cb2824cbd2c03661bfbe62779cec86cc2
XuXinJie <vv19950429@gmail.com>
2024-04-03 14:14:53 +0800

    (調整) 修正資料夾結構

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "@/components//Footer/Footer.tsx";
+import Header from "@/components/Header/Header.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Header />
     <App />
+    <Footer />
   </BrowserRouter>
 );
