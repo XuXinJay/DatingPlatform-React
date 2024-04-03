a239c89e4e88922ca857a549f0a92d551492038b
XuXinJie <vv19950429@gmail.com>
2024-04-03 12:04:40 +0800

    (調整)  首頁搬遷 初始建設

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const addCount = () => {
-    setCount(count + 1);
-  };
-
   return (
-    <>
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4 }}>
-          <Typography
-            variant="h4"
-            className="text-[red]"
-            component="h1"
-            sx={{ mb: 2 }}
-          >
-            Material UI Vite.js example in TypeScript
-            <br />
-            <p className="text-[#fff]">{count}</p>
-          </Typography>
-        </Box>
-        <button type="button" onClick={addCount}>
-          點我
-        </button>
-      </Container>
-    </>
+    <div className="App">
+      <h1>Hello</h1>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
